62963d347be7b2533fe883dd0ec1161e05107045
Dheenu S <dheendheenu777@gmail.com>
2025-01-04 16:22:43 +0530

    commit with new changes in ui

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,9 +1,9 @@
+import EmptyImage from '../assets/emptyimage.svg';
+
 import { useState } from "react";
 
-const initalLists = ['Hi Dev\'s, Dheen Here'] 
-
 function Todo() {
-  const [todo, setTodo] = useState(initalLists);
+  const [todo, setTodo] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [editingIndex, setEditingIndex] = useState(null);
   const [editValue, setEditValue] = useState("");
@@ -45,105 +45,103 @@ function Todo() {
   };
 
   return (
-    <div className="bg-[#222] h-screen p-4">
-      <div className="bg-gray-50 rounded-md flex p-2 gap-2 w-[400px] mx-auto mt-14">
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder="Add new task"
-          onKeyDown={(e) =>
-            e.key === "Enter" && addTodo() // Save when Enter is pressed
-          }
-          className="border border-gray-400 rounded w-full px-2 focus:outline-none focus:border-gray-600"
-          autoFocus
-        />
-        <button
-          onClick={addTodo}
-          className="bg-blue-500 px-2 py-1.5 rounded text-white text-nowrap"
-        >
-          Add Item
-        </button>
-      </div>
-
-      <div className="mt-4 bg-gray-50 px-4 py-2 max-w-[400px] rounded min-h-[400px] max-h-[450px] overflow-y-scroll mx-auto">
-        <div className="relative">
-          {todo.length === 0 ? (
-            <p className="sticky text-[16px] text-gray-400 mt-[10em] ml-[5.8em]">
-              ( Task is Empty! Add new Task )
-            </p>
-          ) : (
-            <div className="border-b pb-2 border-gray-300">
-              <div className="text-left flex items-center gap-2 bg-gray-500 rounded px-2 py-1.5 w-fit text-gray-50">
-                Total Task: <p className="font-medium">{todo.length}</p>
-              </div>
-            </div>
-          )}
+    <div className="bg-[#222] flex flex-col items-center overflow-x-hidden h-screen">
+      <div className="flex flex-col items-center justify-center w-full px-4">
+        <div className="bg-gray-50 rounded-md flex p-2 gap-2 w-full sm:w-[650px] md:w-[800px] mt-14">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Add a new task"
+            onKeyDown={(e) => e.key === "Enter" && addTodo()} // Save when Enter is pressed
+            className="border border-gray-400 rounded w-full px-2 py-1 focus:outline-none focus:border-[#1b9b85]"
+            autoFocus
+          />
+          <button
+            onClick={addTodo}
+            className="bg-[#1a9e88] px-4 py-2 rounded text-white text-nowrap active:scale-95"
+          >
+            Add Task
+          </button>
         </div>
 
-        {todo.map((item, index) => (
-          <div key={index} className="border-b border-gray-300 py-2">
-            {editingIndex === index ? (
-              <div className="bg-gray-100 flex justify-between gap-2">
-                <div className="inline-flex items-center gap-1">
-                  {/* <p>{index + 1})</p> */}
-                  <input
-                    type="text"
-                    value={editValue}
-                    onChange={(e) => setEditValue(e.target.value)}
-                    // onBlur={saveTodo} // Save when focus is lost
-                    onKeyDown={(e) =>
-                      e.key === "Enter" && saveTodo() // Save when Enter is pressed
-                    }
-                    autoFocus
-                    className="border border-gray-400 rounded p-1.5 w-full focus:outline-none focus:border-gray-600"
-                  />
-                </div>
-                <div className="inline-flex gap-2">
-                  <button
-                    onClick={saveTodo}
-                    className="bg-[#008916] px-4 py-1.5 rounded text-white"
-                  >
-                    Save
-                  </button>
-                  <button
-                    onClick={() => deleteTodo(index)}
-                    className="bg-[#DC143C] px-4 py-1.5 rounded text-white"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
+        <div className="mt-4 bg-gray-50 px-4 py-2 w-full sm:w-[650px] md:w-[800px] rounded min-h-[400px] max-h-[450px] overflow-y-scroll mx-4">
+          <div className="">
+            {todo.length === 0 ? (
+              <>
+              <p className=" text-[16px] text-gray-400 text-center mt-4">
+                ( Task is Empty! Add a new Task )
+              </p>
+              <img src={EmptyImage} alt="Empty task" className='mt-8'/>
+              </>
             ) : (
-              <div className="flex items-center justify-between gap-2">
-                <div className="flex space-x-2">
-                  {/* <p>{index + 1})</p> */}
-                  <p
-                    onClick={() => handleEdit(index)}
-                    className="font-normal cursor-pointer"
-                  >
-                    {item}
-                  </p>
-                </div>
-
-                <div className="inline-flex gap-2">
-                  <button
-                    onClick={() => handleEdit(index)}
-                    className="bg-[#008916] px-4 py-1.5 rounded text-white"
-                  >
-                    Edit
-                  </button>
-                  <button
-                    onClick={() => deleteTodo(index)}
-                    className="bg-[#DC143C] px-4 py-1.5 rounded text-white"
-                  >
-                    Delete
-                  </button>
+              <div className="border-b pb-2 border-gray-300">
+                <div className="text-left flex items-center gap-2 rounded-full py-1.5 w-fit text-gray-600">
+                  Total: <p className="font-medium text-[16px] text-[#1a9e88]">{todo.length}</p>
                 </div>
               </div>
             )}
           </div>
-        ))}
+
+          {todo.map((item, index) => (
+            <div key={index} className="border-b border-gray-300 py-2 w-full pl-2">
+              {editingIndex === index ? (
+                <div className="bg-gray-100 flex justify-between gap-2 w-full">
+                  <div className="inline-flex items-center gap-1">
+                    <input
+                      type="text"
+                      value={editValue}
+                      onChange={(e) => setEditValue(e.target.value)}
+                      onKeyDown={(e) => e.key === "Enter" && saveTodo()} // Save when Enter is pressed
+                      autoFocus
+                      className="border border-gray-400 rounded p-1.5 w-full focus:outline-none focus:border-[#1b9b85]"
+                    />
+                  </div>
+                  <div className="inline-flex gap-2">
+                    <button
+                      onClick={saveTodo}
+                      className="bg-[#1a9e88] px-4 py-2 rounded text-white active:scale-95"
+                    >
+                      Save
+                    </button>
+                    <button
+                      onClick={() => deleteTodo(index)}
+                      className="bg-red-600 hover:bg-red-500 active:scale-95 px-4 py-2 rounded text-white"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex items-center justify-between gap-2">
+                  <div className="flex space-x-2">
+                    <p
+                      onClick={() => handleEdit(index)}
+                      className="font-normal cursor-pointer"
+                    >
+                      {item}
+                    </p>
+                  </div>
+
+                  <div className="inline-flex gap-2">
+                    <button
+                      onClick={() => handleEdit(index)}
+                      className="bg-gray-500 px-4 py-1.5 rounded text-white"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => deleteTodo(index)}
+                      className="bg-red-600 hover:bg-red-500 active:scale-95 px-4 py-2 rounded text-white"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
